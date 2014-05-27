18f13d8ee0fe75715b0c4833fc5a7786cb9387c3
DAM1 <DAM1@PC308>
2014-05-27 12:24:27 +0200

    bayer y medicamento dejados a cero para actualizar por el realizado en clase

--- src/com/dam/practica/Bayer.java
+++ src/com/dam/practica/Bayer.java
@@ -5,23 +5,13 @@
 package com.dam.practica;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
  * @author DAM1
  */
 public class Bayer {
-    private ArrayList<Medicamento> listado = new ArrayList<>();
-    public boolean añadirMedicamento(String nombre,int precio,int unidades, int fechaFab, int fechaCad,String principioAct, int tipo, int miligramos){
-        Medicamento m = new Medicamento(nombre, principioAct, precio, tipo, fechaFab, fechaCad, unidades,miligramos);
-        if (listado.contains(m)){
-            int aux = listado.indexOf(m);
-            //busco la posicion del medicamento que ya se encuentra en el listado
-            //para poder actualizar el numero de unidades de ese medicamento
-            //sin necesidad de editar otra vez su nombre, nose como pasarlo
-            return false;
-        }
-        else listado.add(m); return true;
-    }
+  
     
 }
--- src/com/dam/practica/Medicamento.java
+++ src/com/dam/practica/Medicamento.java
@@ -9,20 +9,8 @@ package com.dam.practica;
  * @author DAM1
  */
 public class Medicamento {
-    String nombre, principioActivo;
-    double precio;
-    int  tipo,fechaFab,fechaCad,miligramos,unidades;
-
-    public Medicamento(String nombre, String principioActivo, double precio, int tipo, int fechaFab, int fechaCad, int miligramos, int unidades) {
-        this.nombre = nombre;
-        this.principioActivo = principioActivo;
-        this.precio = precio;
-        this.tipo = tipo;
-        this.fechaFab = fechaFab;
-        this.fechaCad = fechaCad;
-        this.miligramos = miligramos;
-        this.unidades = unidades;
-    }
+    
+    
     
     
     
